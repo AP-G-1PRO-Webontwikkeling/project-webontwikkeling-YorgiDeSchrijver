61e92340ea33b07a387028f30c94e38584c268be
Yorgi De Schrijver <yorgi.deschrijver@student.ap.be>
2024-04-14 11:18:48 +0000

    added index page

--- index.ts
+++ index.ts
@@ -2,9 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
 app.set("port", 3000);
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 
 app.listen(app.get("port"), () => {
